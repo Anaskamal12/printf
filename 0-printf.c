2e8f70ebae15b0cc177b8914c401663f7d7a4625
Anaskamal12 <anaskamal748@gmail.com>
2023-11-14 01:09:00 -0800

    0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -35,11 +35,28 @@ int _printf(const char *format, ...)
 			anas += write(1, str, strlen(str));
 			format++;
 		}
-		else if (*(format + 1) == '%')
+		else if (*(format + 1) == 'd' || *(format + 1) == 'i')
+		{
+			int num = va_arg(mo, int);
+			int temp = num;
+			int len = 1;
+
+			while (temp /= 10)
+			{
+				len++; }
+			char buffer[12];
+			char *ptr = buffer + len;
+
+			do {
+				*--ptr = num % 10 + '0';
+				num /= 10;
+			} while (num);
+			anas += write(1, ptr, len);
+			format++;
+		} else if (*(format + 1) == '%')
 		{
 			anas += write(1, "%", 1);
-			format++;
-		}
+			format++; }
 		format++;
 	}
 	va_end(mo);
