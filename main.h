dc665a4e81182e84af9acc52ae4c54ee56245162
Anaskamal12 <anaskamal748@gmail.com>
2023-11-14 04:13:37 -0800

    2-printf.c

--- main.h
+++ main.h
@@ -9,12 +9,19 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+
 int mustafa_d(int n);
 int mustafa_b(int n);
 int mustafa_i(int n);
 int mustafa_R(char *str);
 int mustafa_r(char *str);
+
 int mustafa_chars(va_list mo, const char *format);
+
+int mustafa_u(unsigned int n);
+int mustafa_o(unsigned int n);
+int mustafa_x(unsigned int n);
+int mustafa_X(unsigned int n);
 
 
 #endif /* MAIN_H */
